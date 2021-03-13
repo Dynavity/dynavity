73fb177862e164cb44e0b2b14e807bd185d5979b
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2021-03-13 17:28:24 +0800

    Setup project

---
+++ Dynavity/Dynavity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dynavity
+//
+//  Created by Hans Sebastian Tirtaputra on 13/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dynavity/Dynavity/DynavityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynavityApp.swift
+//  Dynavity
+//
+//  Created by Hans Sebastian Tirtaputra on 13/3/21.
+//
+
+import SwiftUI
+
+@main
+struct DynavityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
