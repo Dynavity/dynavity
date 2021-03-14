307b8f572294b83417df87c833507ea98920fca0
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2021-03-15 00:06:09 +0800

    Add test canvas element

---
+++ Dynavity/Dynavity/model/TestCanvasElement.swift
@@ -0,0 +1,17 @@
+import Foundation
+import CoreGraphics
+
+/**
+ Test element to use when creating a canvas. This struct can be removed when real canvas elements are created.
+ */
+struct TestCanvasElement: CanvasElementProtocol, Identifiable {
+    typealias elementID = UUID
+    
+    var id: elementID = elementID()
+    var position: CGPoint = .zero
+    var text: String = "Test Canvas Element"
+    
+    static func == (lhs: TestCanvasElement, rhs: TestCanvasElement) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
