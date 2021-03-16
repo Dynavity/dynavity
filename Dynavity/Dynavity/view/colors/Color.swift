739a49afb7c4d0381cea5da2867123045bd6a423
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-16 21:05:36 +0800

    Add colours

---
+++ Dynavity/Dynavity/view/colors/Color.swift
@@ -0,0 +1,22 @@
+// Referenced from https://stackoverflow.com/a/65046791
+
+import SwiftUI
+
+extension Color {
+    struct UI {
+        static let background = Color("bg")
+        static let foreground = Color("fg")
+        static let grey = Color("grey")
+        static let red = Color("red")
+        static let orange = Color("orange")
+        static let green = Color("green")
+        static let teal = Color("teal")
+        static let yellow = Color("yellow")
+        static let blue = Color("blue")
+        static let darkBlue = Color("dark-blue")
+        static let magenta = Color("magenta")
+        static let violet = Color("violet")
+        static let cyan = Color("cyan")
+        static let darkCyan = Color("dark-cyan")
+    }
+}
