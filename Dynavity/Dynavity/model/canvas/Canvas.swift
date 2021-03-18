7894f5ec078864235c39a1ae25c5060a1efd8364
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-18 21:02:37 +0800

    Change Canvas to a struct.
    Rationale is because SwiftUI does not support nested ObservableObjects
    which results in the CanvasElementMap not re-rendering even though the
    underlying canvasElements has been updated.
    
    References:
    https://stackoverflow.com/questions/61280353/change-on-observableobject-in-published-array-does-not-update-the-view
    https://stackoverflow.com/questions/58406287/how-to-tell-swiftui-views-to-bind-to-nested-observableobjects

--- Dynavity/Dynavity/model/canvas/Canvas.swift
+++ Dynavity/Dynavity/model/canvas/Canvas.swift
@@ -1,8 +1,8 @@
 import Foundation
 import CoreGraphics
 
-class Canvas: ObservableObject {
-    @Published var canvasElements: [CanvasElementProtocol] = []
+struct Canvas {
+    var canvasElements: [CanvasElementProtocol] = []
 
     init() {
         let testElement1 = TestCanvasElement(position: CGPoint(x: -150, y: -150), text: "Test1")
@@ -11,7 +11,7 @@ class Canvas: ObservableObject {
         addElement(testElement2)
     }
 
-    func addElement(_ element: CanvasElementProtocol) {
+    mutating func addElement(_ element: CanvasElementProtocol) {
         canvasElements.append(element)
     }
 }
