f6a6bd6e2d4b1129f5979d602c80c7b9cb8bae6d
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2021-03-19 13:09:08 +0800

    Modify test elements in Canvas to account for new Canvas starting location

--- Dynavity/Dynavity/model/canvas/Canvas.swift
+++ Dynavity/Dynavity/model/canvas/Canvas.swift
@@ -5,8 +5,9 @@ class Canvas: ObservableObject {
     @Published var canvasElements: [CanvasElementProtocol] = []
 
     init() {
-        let testElement1 = TestCanvasElement(position: CGPoint(x: -150, y: -150), text: "Test1")
-        let testElement2 = TestCanvasElement(position: CGPoint(x: 150, y: 150), text: "Test2")
+        /// Starting center is now (x: 250000, y:  250000) 
+        let testElement1 = TestCanvasElement(position: CGPoint(x: 250_000, y: 250_000), text: "Test1")
+        let testElement2 = TestCanvasElement(position: CGPoint(x: 250_500, y: 250_500), text: "Test2")
         addElement(testElement1)
         addElement(testElement2)
     }
