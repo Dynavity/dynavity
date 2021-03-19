5babda2f8bb02e8d9239571dc407767387f6178a
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-19 20:56:38 +0800

    Add searching of canvases

--- Dynavity/Dynavity/view/HomeView.swift
+++ Dynavity/Dynavity/view/HomeView.swift
@@ -5,10 +5,19 @@ struct HomeView: View {
     @State var canvases: [String] = (1...100).map { "Canvas \($0)" }
     @State var searchQuery: String = ""
 
+    init() {
+        self.canvases = self.canvases.filter {
+            if searchQuery.isEmpty {
+                return true
+            }
+            return $0.lowercased().contains(self.searchQuery.lowercased())
+        }
+    }
+
     var body: some View {
-        SearchBar(text: .constant(""))
+        SearchBar(text: $searchQuery)
             .padding()
-        CanvasSelectionView(canvases: $canvases)
+        CanvasSelectionView(canvases: $canvases, searchQuery: $searchQuery)
     }
 }
 
