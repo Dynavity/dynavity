7d5ff2cd3d70a63efa2ff3ec3705a76fc65d5e05
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-19 21:33:02 +0800

    remove unused code

--- Dynavity/Dynavity/view/HomeView.swift
+++ Dynavity/Dynavity/view/HomeView.swift
@@ -5,15 +5,6 @@ struct HomeView: View {
     @State var canvases: [String] = (1...100).map { "Canvas \($0)" }
     @State var searchQuery: String = ""
 
-    init() {
-        self.canvases = self.canvases.filter {
-            if searchQuery.isEmpty {
-                return true
-            }
-            return $0.lowercased().contains(self.searchQuery.lowercased())
-        }
-    }
-
     var body: some View {
         SearchBar(text: $searchQuery)
             .padding()
