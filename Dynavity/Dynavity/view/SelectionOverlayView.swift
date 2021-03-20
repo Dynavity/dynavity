bc36c96fb1fd2fdacfe65d6585e6d4ddadd89f32
Ian Yong <ianyongyc@gmail.com>
2021-03-21 02:04:54 +0800

    Add rotation control to SelectionOverlayView

--- Dynavity/Dynavity/view/SelectionOverlayView.swift
+++ Dynavity/Dynavity/view/SelectionOverlayView.swift
@@ -3,12 +3,34 @@ import SwiftUI
 struct SelectionOverlayView: View {
     var element: CanvasElementProtocol
 
+    private let rotationControlSize: CGFloat = 20.0
+    private let rotationControlHandleLength: CGFloat = 10.0
+
+    private var rotationControl: some View {
+        VStack(spacing: .zero) {
+            ZStack {
+                Circle()
+                    .fill(Color.white)
+                Image(systemName: "arrow.triangle.2.circlepath.circle")
+                    .resizable()
+            }
+            .frame(width: rotationControlSize, height: rotationControlSize)
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 1.0, height: rotationControlHandleLength)
+        }
+        .offset(y: -(element.height + rotationControlSize + rotationControlHandleLength) / 2.0)
+    }
+
     var body: some View {
-        Rectangle()
-            .fill(Color.green)
-            .frame(width: element.width,
-                   height: element.height,
-                   alignment: .center)
+        ZStack {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: element.width,
+                       height: element.height,
+                       alignment: .center)
+            rotationControl
+        }
     }
 }
 
