24ef9c1097246bcc381d6bd51b9fef6dec1a26cd
Ian Yong <ianyongyc@gmail.com>
2021-03-20 20:32:46 +0800

    Fix calculation of offset when zoom != 1.0

--- Dynavity/Dynavity/view/canvas/CanvasView.swift
+++ Dynavity/Dynavity/view/canvas/CanvasView.swift
@@ -3,15 +3,27 @@ import SwiftUI
 struct CanvasView: View {
     @ObservedObject var viewModel: CanvasViewModel
 
+    private func calculateOffsetX(width: CGFloat) -> CGFloat {
+        -(viewModel.canvasOrigin.x + viewModel.canvasCenterOffsetX)
+            + width / 2.0 * (viewModel.scaleFactor - 1.0)
+    }
+
+    private func calculateOffsetY(height: CGFloat) -> CGFloat {
+        -(viewModel.canvasOrigin.y + viewModel.canvasCenterOffsetY)
+            + height / 2.0 * (viewModel.scaleFactor - 1.0)
+    }
+
     var body: some View {
-        ZStack {
-            AnnotationCanvasView(viewModel: viewModel)
-            CanvasElementMapView(viewModel: viewModel)
-                .scaleEffect(viewModel.scaleFactor)
-                .offset(
-                    x: -(viewModel.canvasOrigin.x + viewModel.canvasCenterOffsetX),
-                    y: -(viewModel.canvasOrigin.y + viewModel.canvasCenterOffsetY)
-                )
+        GeometryReader { geometry in
+            ZStack {
+                AnnotationCanvasView(viewModel: viewModel)
+                CanvasElementMapView(viewModel: viewModel)
+                    .scaleEffect(viewModel.scaleFactor)
+                    .offset(
+                        x: calculateOffsetX(width: geometry.size.width),
+                        y: calculateOffsetY(height: geometry.size.height)
+                    )
+            }
         }
     }
 }
