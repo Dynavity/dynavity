f6674fab5fba0a66e502d738145b3b7f07c0d086
Ian Yong <ianyongyc@gmail.com>
2021-03-21 15:10:41 +0800

    Add resize controls to SelectionOverlayView

--- Dynavity/Dynavity/view/SelectionOverlayView.swift
+++ Dynavity/Dynavity/view/SelectionOverlayView.swift
@@ -6,9 +6,29 @@ struct SelectionOverlayView: View {
 
     private let rotationControlSize: CGFloat = 25.0
     private let rotationControlHandleLength: CGFloat = 10.0
+    private let resizeControlSize: CGFloat = 15.0
+    private let resizeControlBorderPercentage: CGFloat = 0.1
 
     private var rotationControlOffset: CGFloat {
         -(element.height * viewModel.scaleFactor + rotationControlSize + rotationControlHandleLength) / 2.0
+    }
+    private var halfWidth: CGFloat {
+        element.width / 2.0
+    }
+    private var halfHeight: CGFloat {
+        element.height / 2.0
+    }
+    private var topLeftCorner: CGSize {
+        CGSize(width: -halfWidth, height: -halfHeight)
+    }
+    private var topRightCorner: CGSize {
+        CGSize(width: halfWidth, height: -halfHeight)
+    }
+    private var bottomLeftCorner: CGSize {
+        CGSize(width: -halfWidth, height: halfHeight)
+    }
+    private var bottomRightCorner: CGSize {
+        CGSize(width: halfWidth, height: halfHeight)
     }
 
     private var rotationGesture: some Gesture {
@@ -40,6 +60,15 @@ struct SelectionOverlayView: View {
         .scaleEffect(1.0 / viewModel.scaleFactor)
     }
 
+    private var resizeControl: some View {
+        Rectangle()
+            .fill(Color.white)
+            .frame(width: resizeControlSize, height: resizeControlSize)
+            .border(Color.blue, width: resizeControlSize * resizeControlBorderPercentage)
+            // Force the resize control to be the same size regardless of scale factor.
+            .scaleEffect(1.0 / viewModel.scaleFactor)
+    }
+
     var body: some View {
         ZStack {
             Rectangle()
@@ -49,6 +78,14 @@ struct SelectionOverlayView: View {
                        alignment: .center)
                 .allowsHitTesting(false)
             rotationControl
+            resizeControl
+                .offset(topLeftCorner)
+            resizeControl
+                .offset(topRightCorner)
+            resizeControl
+                .offset(bottomLeftCorner)
+            resizeControl
+                .offset(bottomRightCorner)
         }
     }
 }
