2846dd19f4b22dd5ef8c94fb50c9dac53a2d73fb
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-27 18:47:24 +0800

    Add dragging gesture for viewport

--- Dynavity/Dynavity/view/graph/GraphView.swift
+++ Dynavity/Dynavity/view/graph/GraphView.swift
@@ -3,12 +3,30 @@ import SwiftUI
 struct GraphView: View {
     @StateObject var viewModel = GraphMapViewModel()
 
+    // For viewport dragging gesture
+    @State var originOffset: CGPoint = .zero
+    @State var dragOffset: CGSize = .zero
+
     var body: some View {
-        ZStack {
+        GeometryReader { _ in
+            ZStack {
+                Rectangle().fill(Color.UI.base5)
+                graphView
+            }
+            .drawingGroup(opaque: true, colorMode: .extendedLinear)
+            .gesture(viewportDragGesture)
+        }
+    }
+
+    var graphView: some View {
+        Group {
             nodesView
                 .zIndex(.infinity)
             edgesView
         }
+        .offset(x: self.originOffset.x + self.dragOffset.width,
+                y: self.originOffset.y + self.dragOffset.height)
+        .animation(.easeIn)
     }
 
     var nodesView: some View {
@@ -23,6 +41,20 @@ struct GraphView: View {
             EdgeView(start: edge.source.position, end: edge.destination.position)
                 .stroke()
         }
+    }
+}
+
+// MARK: Gestures
+extension GraphView {
+    var viewportDragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                dragOffset = value.translation
+            }
+            .onEnded { value in
+                dragOffset = .zero
+                originOffset += value.translation
+            }
     }
 }
 
