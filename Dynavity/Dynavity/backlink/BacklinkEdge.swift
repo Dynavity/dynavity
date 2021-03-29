a4fef39f13b762a8e94071156ef5a2ccb7d56c89
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-03-29 23:34:23 +0800

    Fix choppy edge rendering

--- Dynavity/Dynavity/backlink/BacklinkEdge.swift
+++ Dynavity/Dynavity/backlink/BacklinkEdge.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 struct BacklinkEdge {
-    var id = UUID()
+    // TODO: there's probably a better way of generating an ID
+    // from the IDs of source and destination
+    var id: Int {
+        self.hashValue
+    }
     let source: BacklinkNode
     let destination: BacklinkNode
 }
 
-extension BacklinkEdge: Equatable {
+extension BacklinkEdge: Hashable {
     /// Two BacklinkEdge are equal if:
     /// 1. Their source and destination nodes are the same
     /// 2. Their source and destination nodes are the opposite of each other's
