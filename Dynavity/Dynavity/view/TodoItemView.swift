e75232fa64aa5a6e2c214d71cf20538136a0dc76
Ian Yong <ianyongyc@gmail.com>
2021-03-31 22:37:53 +0800

    Add swipe gesture to todos

--- Dynavity/Dynavity/view/TodoItemView.swift
+++ Dynavity/Dynavity/view/TodoItemView.swift
@@ -3,11 +3,34 @@ import SwiftUI
 struct TodoItemView: View {
     static let checkboxSize: CGFloat = 35.0
     static let checkboxBorder: CGFloat = 1.0
+    static let swipeOffsetThreshold: CGFloat = -50.0
+    static let swipeInertia: CGFloat = 10.0
 
     @StateObject private var viewModel: TodoItemViewModel
+    @State private var swipeOffset: CGFloat = .zero
 
     init(todo: Todo) {
         self._viewModel = StateObject(wrappedValue: TodoItemViewModel(todo: todo))
+    }
+
+    private var swipeGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                var updatedSwipeOffset = value.translation.width
+                if updatedSwipeOffset > .zero {
+                    updatedSwipeOffset /= TodoItemView.swipeInertia
+                }
+                swipeOffset = updatedSwipeOffset
+            }
+            .onEnded { _ in
+                withAnimation {
+                    if swipeOffset < TodoItemView.swipeOffsetThreshold {
+                        swipeOffset = TodoItemView.swipeOffsetThreshold
+                    } else {
+                        swipeOffset = 0.0
+                    }
+                }
+            }
     }
 
     private var checkbox: some View {
@@ -40,6 +63,8 @@ struct TodoItemView: View {
             TextField("", text: $viewModel.todo.label)
                 .foregroundColor(viewModel.todo.isCompleted ? Color.gray : Color.black)
         }
+        .offset(x: swipeOffset)
+        .gesture(swipeGesture)
     }
 }
 
