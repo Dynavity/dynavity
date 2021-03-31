4c8b403b7465cda42a21cd9b36cf05ecbfb44467
Ian Yong <ianyongyc@gmail.com>
2021-03-31 22:19:01 +0800

    Set minimum width and height of TodoElement

--- Dynavity/Dynavity/model/canvas/elements/TodoElement.swift
+++ Dynavity/Dynavity/model/canvas/elements/TodoElement.swift
@@ -7,6 +7,12 @@ struct TodoElement: CanvasElementProtocol {
     var width: CGFloat = 500.0
     var height: CGFloat = 500.0
     var rotation: Double = .zero
+    var minimumWidth: CGFloat {
+        240.0
+    }
+    var minimumHeight: CGFloat {
+        60.0
+    }
 
     // MARK: TodosElement-specific attributes
     var todos: [Todo] = []
