c9de87a9902410f86f9f3425138e743fb5c07e65
Ian Yong <ianyongyc@gmail.com>
2021-03-31 21:36:36 +0800

    Add checkbox to todo

--- Dynavity/Dynavity/view/TodoItemView.swift
+++ Dynavity/Dynavity/view/TodoItemView.swift
@@ -1,11 +1,33 @@
 import SwiftUI
 
 struct TodoItemView: View {
-    var todo: Todo
+    static let checkboxSize: CGFloat = 30.0
+    static let checkboxBorder: CGFloat = 1.0
+
+    @StateObject private var viewModel: TodoItemViewModel
+
+    init(todo: Todo) {
+        self._viewModel = StateObject(wrappedValue: TodoItemViewModel(todo: todo))
+    }
+
+    private var checkbox: some View {
+        Button(action: {
+            viewModel.toggleIsCompleted()
+        }) {
+            Circle()
+                .strokeBorder(Color.gray, lineWidth: TodoItemView.checkboxBorder)
+                .background(
+                    Circle()
+                        .foregroundColor(viewModel.todo.isCompleted ? Color.green : Color.clear)
+                )
+                .frame(width: TodoItemView.checkboxSize, height: TodoItemView.checkboxSize)
+        }
+    }
 
     var body: some View {
         HStack {
-            Text(todo.label)
+            checkbox
+            Text(viewModel.todo.label)
         }
     }
 }
