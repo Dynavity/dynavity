7e6f79f2806e04c530e3ffa917e17ea90b3d502c
Ian Yong <ianyongyc@gmail.com>
2021-04-01 00:59:07 +0800

    Remove test todo data

--- Dynavity/Dynavity/view-model/canvas/elements/TodoElementViewModel.swift
+++ Dynavity/Dynavity/view-model/canvas/elements/TodoElementViewModel.swift
@@ -9,14 +9,6 @@ class TodoElementViewModel: ObservableObject {
 
     init(todoElement: TodoElement) {
         self.todoElement = todoElement
-        initialiseTestTodos()
-    }
-
-    // TODO: Remove this function.
-    private func initialiseTestTodos() {
-        for i in 1..<20 {
-            todoElement.todos.append(Todo(label: "This is Todo #\(i)"))
-        }
     }
 
     func removeTodo(_ todo: Todo) {
