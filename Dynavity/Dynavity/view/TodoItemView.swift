395d3a7d9d8d9c772eebbc896cbc527ecb90cc7c
Ian Yong <ianyongyc@gmail.com>
2021-03-31 22:11:00 +0800

    Add animation when selecting checkbox

--- Dynavity/Dynavity/view/TodoItemView.swift
+++ Dynavity/Dynavity/view/TodoItemView.swift
@@ -12,21 +12,31 @@ struct TodoItemView: View {
 
     private var checkbox: some View {
         Button(action: {
-            viewModel.toggleIsCompleted()
+            withAnimation(Animation.easeInOut(duration: 0.5)) {
+                viewModel.toggleIsCompleted()
+            }
         }) {
-            Circle()
-                .strokeBorder(Color.gray, lineWidth: TodoItemView.checkboxBorder)
-                .background(
-                    Circle()
-                        .foregroundColor(viewModel.todo.isCompleted ? Color.green : Color.clear)
-                )
-                .frame(width: TodoItemView.checkboxSize, height: TodoItemView.checkboxSize)
+            ZStack {
+                Circle()
+                    .strokeBorder(Color.gray, lineWidth: TodoItemView.checkboxBorder)
+                    .background(
+                        Circle()
+                            .foregroundColor(viewModel.todo.isCompleted ? Color.green : Color.clear)
+                    )
+                    .frame(width: TodoItemView.checkboxSize, height: TodoItemView.checkboxSize)
+                Image(systemName: "checkmark")
+                    .foregroundColor(Color.white)
+                    .opacity(viewModel.todo.isCompleted ? 1.0 : 0.0)
+            }
         }
+        // Disable flashing when tapped.
+        .buttonStyle(PlainButtonStyle())
     }
 
     var body: some View {
         HStack {
             checkbox
+                .padding(.leading, 5.0)
             TextEditor(text: $viewModel.todo.label)
         }
     }
