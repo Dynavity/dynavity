3ac59e5b6e0d540c85595e615ee4e3b031cfe64f
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-04-02 22:48:25 +0800

    Add iPhone as user agent for WebView and add HTML template

--- Dynavity/Dynavity/view/WebView.swift
+++ Dynavity/Dynavity/view/WebView.swift
@@ -2,19 +2,35 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let rawHtml: String
+    let bodyHtmlContent: String
 
     func makeUIView(context: Context) -> WKWebView {
-        WKWebView()
+        let view = WKWebView()
+        // iPhone X/XS iOS 12
+        view.customUserAgent = """
+            Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) \
+            Version/12.0 Mobile/15A372 Safari/604.1
+        """
+        return view
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
+        let rawHtml = """
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+
+            <body>
+            \(bodyHtmlContent)
+            </body>
+        """
         uiView.loadHTMLString(rawHtml, baseURL: nil)
     }
 }
 
 struct WebView_Previews: PreviewProvider {
     static var previews: some View {
-        WebView(rawHtml: "<html><body><p>Hello!</p></body></html><ul><li>Cool html</li><li>More cool html</li></ul>")
+        WebView(bodyHtmlContent: "<p>Hello!</p><ul><li>Cool html</li><li>More cool html</li></ul>")
     }
 }
