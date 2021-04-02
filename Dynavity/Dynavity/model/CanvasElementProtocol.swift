53e2dde65bf02bd10ac5c909b47c4c2f1a69d4fb
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2021-04-02 22:27:12 +0800

    Add rotation support for UML connector

--- Dynavity/Dynavity/model/CanvasElementProtocol.swift
+++ Dynavity/Dynavity/model/CanvasElementProtocol.swift
@@ -24,6 +24,56 @@ extension CanvasElementProtocol {
         30.0
     }
 
+    // Corners are with reference to 0 rotation. Corners are consistent throughout rotation.
+    // For example, topRightCorner might be at bottom left in view when element is rotated
+    var topRightCorner: CGPoint {
+        let xPosition = position.x + ((width / 2) * CGFloat(cos(rotation))) + ((height / 2) * CGFloat(sin(rotation)))
+        let yPosition = position.y + ((width / 2) * CGFloat(sin(rotation))) - ((height / 2) * CGFloat(cos(rotation)))
+        return CGPoint(x: xPosition,
+                       y: yPosition)
+    }
+
+    var topLeftCorner: CGPoint {
+        let xPosition = position.x - ((width / 2) * CGFloat(cos(rotation))) + ((height / 2) * CGFloat(sin(rotation)))
+        let yPosition = position.y - ((width / 2) * CGFloat(sin(rotation))) - ((height / 2) * CGFloat(cos(rotation)))
+        return CGPoint(x: xPosition,
+                       y: yPosition)
+    }
+
+    var bottomLeftCorner: CGPoint {
+        let xPosition = position.x - ((width / 2) * CGFloat(cos(rotation))) - ((height / 2) * CGFloat(sin(rotation)))
+        let yPosition = position.y - ((width / 2) * CGFloat(sin(rotation))) + ((height / 2) * CGFloat(cos(rotation)))
+        return CGPoint(x: xPosition,
+                       y: yPosition)
+    }
+
+    var bottomRightCorner: CGPoint {
+        let xPosition = position.x + ((width / 2) * CGFloat(cos(rotation))) - ((height / 2) * CGFloat(sin(rotation)))
+        let yPosition = position.y + ((width / 2) * CGFloat(sin(rotation))) + ((height / 2) * CGFloat(cos(rotation)))
+        return CGPoint(x: xPosition,
+                       y: yPosition)
+    }
+
+    var topMostPoint: CGPoint {
+        let points = [topRightCorner, bottomLeftCorner, bottomRightCorner]
+        return points.reduce(topLeftCorner, { $0.y < $1.y ? $0 : $1 })
+    }
+
+    var bottomMostPoint: CGPoint {
+        let points = [topRightCorner, bottomLeftCorner, bottomRightCorner]
+        return points.reduce(topLeftCorner, { $0.y > $1.y ? $0 : $1 })
+    }
+
+    var leftMostPoint: CGPoint {
+        let points = [topRightCorner, bottomLeftCorner, bottomRightCorner]
+        return points.reduce(topLeftCorner, { $0.x < $1.x ? $0 : $1 })
+    }
+
+    var rightMostPoint: CGPoint {
+        let points = [topRightCorner, bottomLeftCorner, bottomRightCorner]
+        return points.reduce(topLeftCorner, { $0.x > $1.x ? $0 : $1 })
+    }
+
     mutating func move(by translation: CGSize) {
         self.position += translation
     }
