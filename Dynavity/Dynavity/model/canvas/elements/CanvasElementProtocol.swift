06e6f7666d8434f94d6eee1fbff70dffb65d0f3b
Ian Yong <ianyongyc@gmail.com>
2021-04-06 19:29:57 +0800

    Bridge properties of CanvasElementProperties

--- Dynavity/Dynavity/model/canvas/elements/CanvasElementProtocol.swift
+++ Dynavity/Dynavity/model/canvas/elements/CanvasElementProtocol.swift
@@ -2,7 +2,7 @@ import Foundation
 import CoreGraphics
 
 protocol CanvasElementProtocol: Codable {
-    var canvasProperties: CanvasElementProperties { set get }
+    var canvasProperties: CanvasElementProperties { get set }
 
     mutating func move(by translation: CGSize)
     mutating func resize(by translation: CGSize)
@@ -24,5 +24,72 @@ extension CanvasElementProtocol {
 
     mutating func rotate(to rotation: Double) {
         self.canvasProperties.rotation = rotation
+    }
+}
+
+// MARK: Properties
+extension CanvasElementProtocol {
+    var position: CGPoint {
+        get { canvasProperties.position }
+        set { canvasProperties.position = newValue }
+    }
+
+    var width: CGFloat {
+        get { canvasProperties.width }
+        set { canvasProperties.width = newValue }
+    }
+
+    var height: CGFloat {
+        get { canvasProperties.height }
+        set { canvasProperties.height = newValue }
+    }
+
+    var rotation: Double {
+        get { canvasProperties.rotation }
+        set { canvasProperties.rotation = newValue }
+    }
+
+    var minimumWidth: CGFloat {
+        canvasProperties.minimumWidth
+    }
+
+    var minimumHeight: CGFloat {
+        canvasProperties.minimumHeight
+    }
+
+    var topRightCorner: CGPoint {
+        canvasProperties.topRightCorner
+    }
+
+    var topLeftCorner: CGPoint {
+        canvasProperties.topLeftCorner
+    }
+
+    var bottomLeftCorner: CGPoint {
+        canvasProperties.bottomLeftCorner
+    }
+
+    var bottomRightCorner: CGPoint {
+        canvasProperties.bottomRightCorner
+    }
+
+    var topMostPoint: CGPoint {
+        canvasProperties.topMostPoint
+    }
+
+    var bottomMostPoint: CGPoint {
+        canvasProperties.bottomMostPoint
+    }
+
+    var leftMostPoint: CGPoint {
+        canvasProperties.leftMostPoint
+    }
+
+    var rightMostPoint: CGPoint {
+        canvasProperties.rightMostPoint
+    }
+
+    func containsPoint(_ point: CGPoint) -> Bool {
+        canvasProperties.containsPoint(point)
     }
 }
