159993eac6e460f785b34faa46717444f6f2d17f
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-04-10 22:50:27 +0800

    Refactor Todo to a class

--- Dynavity/Dynavity/model/Todo.swift
+++ Dynavity/Dynavity/model/Todo.swift
@@ -1,10 +1,26 @@
 import SwiftUI
 
-struct Todo {
-    var id = UUID()
+class Todo {
     var label: String
-    var isCompleted = false
+    var isCompleted: Bool
+
+    init(label: String, isCompleted: Bool) {
+        self.label = label
+        self.isCompleted = isCompleted
+    }
+
+    convenience init(label: String) {
+        self.init(label: label, isCompleted: false)
+    }
 }
 
 // MARK: Hashable
-extension Todo: Hashable {}
+extension Todo: Hashable {
+    static func == (lhs: Todo, rhs: Todo) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
