71d929741cd710f596ad96b83bfc98ea3cb495f5
Ian Yong <ianyongyc@gmail.com>
2021-04-11 13:58:18 +0800

    Set up observers for todos

--- Dynavity/Dynavity/model/elements/TodoElement.swift
+++ Dynavity/Dynavity/model/elements/TodoElement.swift
@@ -6,7 +6,8 @@ class TodoElement: ObservableObject, CanvasElementProtocol {
     @Published var canvasProperties: CanvasElementProperties
 
     // MARK: TodosElement-specific attributes
-    var todos: [Todo] = []
+    var todos: [Todo]
+    private var todoCancellables: [AnyCancellable]
 
     init(position: CGPoint) {
         self.canvasProperties = CanvasElementProperties(
@@ -14,13 +15,21 @@ class TodoElement: ObservableObject, CanvasElementProtocol {
             minimumWidth: 240.0,
             minimumHeight: 60.0
         )
+        self.todos = []
+        self.todoCancellables = []
     }
 
     func removeTodo(at index: Int) {
         todos.remove(at: index)
+        todoCancellables.remove(at: index)
     }
 
     func addTodo(label: String) {
-        todos.append(Todo(label: label))
+        let todo = Todo(label: label)
+        todos.append(todo)
+        let cancellable = todo.objectWillChange.sink { [weak self] _ in
+            self?.objectWillChange.send()
+        }
+        todoCancellables.append(cancellable)
     }
 }
