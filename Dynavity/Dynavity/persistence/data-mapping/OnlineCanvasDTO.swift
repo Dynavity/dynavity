e2ac5b8ac729ede078dee6f9aad2c460558c43b9
wltan <e0001590@u.nus.edu>
2021-04-13 22:42:59 +0800

    Remove ID from OnlineCanvasDTO

--- Dynavity/Dynavity/persistence/data-mapping/OnlineCanvasDTO.swift
+++ Dynavity/Dynavity/persistence/data-mapping/OnlineCanvasDTO.swift
@@ -1,19 +1,17 @@
 import Foundation
 
 struct OnlineCanvasDTO: Mappable {
-    let id: UUID
     let ownerId: String
     let canvas: CanvasDTO
 
     init(model: OnlineCanvas) {
-        self.id = model.id
         self.ownerId = model.ownerId
         self.canvas = CanvasDTO(id: model.id,
                                 model: CanvasWithAnnotation(canvas: model, annotationCanvas: AnnotationCanvas()))
     }
 
     func toModel() -> OnlineCanvas {
-        OnlineCanvas(id: id, ownerId: ownerId, canvas: canvas.toModel().canvas)
+        OnlineCanvas(id: canvas.id, ownerId: ownerId, canvas: canvas.toModel().canvas)
     }
 }
 
