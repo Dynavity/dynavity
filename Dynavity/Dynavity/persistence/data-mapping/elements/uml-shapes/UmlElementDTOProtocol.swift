59520c6174eda23b6edeb2131e4d7a83c7ebffd9
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2021-04-18 13:36:33 +0800

    Rename UmlElementProtocolDTO

--- Dynavity/Dynavity/persistence/data-mapping/elements/uml-shapes/UmlElementDTOProtocol.swift
+++ Dynavity/Dynavity/persistence/data-mapping/elements/uml-shapes/UmlElementDTOProtocol.swift
@@ -1,4 +1,4 @@
-protocol UmlElementProtocolDTO: CanvasElementDTOProtocol {
+protocol UmlElementDTOProtocol: CanvasElementDTOProtocol {
     var label: String { get set }
     var umlType: String { get }
     var umlShape: String { get }
